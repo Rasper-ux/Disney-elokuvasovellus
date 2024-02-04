9c5758b8b2600b081b9162eb4282a06e6566473e
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-02-04 12:59:44 +0200

    Add modules

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
 from flask import Flask
-from flask import redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
 from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
-db = SQLAlchemy(app)
+app.secret_key = getenv("SECRET_KEY")
 
-@app.route("/")
-def index():
-    result = db.session.execute(text("SELECT name FROM films"))
-    films = result.fetchall()
-    return render_template("index.html", count=len(films), movies=films)
+import routes
---
+++ films.py
@@ -0,0 +1,8 @@
+from db import db
+import users
+from sqlalchemy.sql import text
+
+def get_list():
+    sql = text("SELECT name, year, runtime FROM films)
+    result = db.session.execute(sql)
+    return result.fetchall()
