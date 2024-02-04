ea00d2d9f459fca93b153c5ca59802328b4bcad7
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-02-04 13:31:25 +0200

    Editing sql search

--- films.py
+++ films.py
@@ -3,6 +3,6 @@ import users
 from sqlalchemy.sql import text
 
 def get_list():
-    sql = text("SELECT name, year, runtime FROM films)
+    sql = text("SELECT name, year, runtime FROM films")
     result = db.session.execute(sql)
     return result.fetchall()
