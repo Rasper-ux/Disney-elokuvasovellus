aebd4642cb8d8854b41745b0f91a8365d34a9804
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-02-07 08:55:02 +0200

    Fixed stars REFERENCE

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TABLE films (id SERIAL PRIMARY KEY, name TEXT, year INTEGER, runtime INTE
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
 CREATE TABLE user_info  (id SERIAL PRIMARY KEY, user_id INREGER REFERENCES users, age INTEGER, favourite_film TEXT);
 CREATE TABLE reviews (id SERIAL PRIMARY KEY, film_id INTEGER REFERENCES film, writer_id INTEGER REFERENCES users, review TEXT);
-CREATE TABLE stars (id SERIAL PRIMARY KEY, film_id INTEGER REFERENCES, writer_id INTEGER REFERENCES users, stars INTEGER);
+CREATE TABLE stars (id SERIAL PRIMARY KEY, film_id INTEGER REFERENCES films, writer_id INTEGER REFERENCES users, stars INTEGER);
 INSERT INTO films (name, year, runtime) VALUES ("Snow White and the Seven Dwarfs", 1937, 83);
 INSERT INTO films (name, year, runtime) VALUES ("Pinocchio", 1940, 88);
 INSERT INTO films (name, year, runtime) VALUES ("Fantasia", 1940, 125);
