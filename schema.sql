fa9f98aaf68d2963fe2c945054283683985a6a3b
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-02-16 14:41:55 +0200

    Add films

--- schema.sql
+++ schema.sql
@@ -12,7 +12,16 @@ INSERT INTO films (name, year, runtime) VALUES ('Bambi', 1942, 69);
 INSERT INTO films (name, year, runtime) VALUES ('Saludos Amigos', 1942, 42);
 INSERT INTO films (name, year, runtime) VALUES ('The Three Caballeros', 1944, 71);
 INSERT INTO films (name, year, runtime) VALUES ('Make Mine Music', 1946, 75);
-
-
+INSERT INTO films (name, year, runtime) VALUES ('The Reluctant Dragon', 1941, 74);
+INSERT INTO films (name, year, runtime) VALUES ('Song of the South', 1946, 94);
+INSERT INTO films (name, year, runtime) VALUES ('Fun & Fancy Free', 1947, 73);
+INSERT INTO films (name, year, runtime) VALUES ('Melody Time', 1948, 75);
+INSERT INTO films (name, year, runtime) VALUES ('So Dear To My Heart', 1948, 82);
+INSERT INTO films (name, year, runtime) VALUES ('The Adventures of Ichabod and Mr. Toad', 1949, 68);
+INSERT INTO films (name, year, runtime) VALUES ('Cinderella', 1950, 74);
+INSERT INTO films (name, year, runtime) VALUES ('Alice in Wonderland', 1951, 75);
+INSERT INTO films (name, year, runtime) VALUES ('Peter Pan', 1953, 77);
+INSERT INTO films (name, year, runtime) VALUES ('Lady and the Tramp', 1955, 76);
+INSERT INTO films (name, year, runtime) VALUES ('Sleeping Beauty', 1959, 75);
 
 
